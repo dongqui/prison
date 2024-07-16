cd5a728ef81bc429a6e2d231aacaf40d49a30fd3
Dongjin <dongqui9@gmail.com>
2024-07-16 21:48:48 +0900

    feat: Avatar

---
+++ src/app/(shared)/components/Avatar.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Avatar() {
+  return (
+    <Image
+      className="w-9 h-9 rounded-full mx-auto"
+      src="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      alt=""
+      width="24"
+      height="24"
+    />
+  );
+}
