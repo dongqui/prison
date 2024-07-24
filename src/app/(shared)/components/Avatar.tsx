a8453f80c2a0077f999808bf7ab328df3fea02c6
Dongjin <dongqui9@gmail.com>
2024-07-24 18:09:05 +0900

    fix: avatar ui

--- src/app/(shared)/components/Avatar.tsx
+++ src/app/(shared)/components/Avatar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Avatar() {
   return (
     <Image
-      className="w-9 h-9 rounded-full mx-auto"
+      className="w-9 h-9 rounded-full"
       src="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       alt=""
       width="24"
