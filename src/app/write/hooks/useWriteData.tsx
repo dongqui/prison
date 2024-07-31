6c139cf85f6601633e536897a642e0bced72e575
Dongjin <dongqui9@gmail.com>
2024-07-31 17:51:55 +0900

    feat: write data validation

--- src/app/write/hooks/useWriteData.tsx
+++ src/app/write/hooks/useWriteData.tsx
@@ -1,7 +1,49 @@
 import { useAtom, atom } from "jotai";
 
-const _atom = atom(0);
+import type { WriteStep } from "@/(shared)/types";
+
+interface MatchUser {
+  puid: string;
+  riotId: string;
+  icon: string;
+  KDA: string;
+}
+
+interface WriteData {
+  match: {
+    id: string;
+    users: MatchUser[];
+  } | null;
+  targetUser: Omit<MatchUser, "KDA"> | null;
+  content: {
+    image: string;
+    text: string;
+  }[];
+  title: string;
+}
+
+const _atom = atom<WriteData>({
+  match: null,
+  targetUser: null,
+  content: [],
+  title: "",
+});
 
 export function useWriteData() {
-  return useAtom(_atom);
+  const [data, setData] = useAtom(_atom);
+
+  function stepDataValidation(step: WriteStep) {
+    switch (step) {
+      case "match":
+        return !!data.match;
+      case "user":
+        return !!data.targetUser;
+      case "content":
+        return !!data.content.length;
+      case "title":
+        return !!data.title;
+    }
+  }
+
+  return { stepDataValidation };
 }
